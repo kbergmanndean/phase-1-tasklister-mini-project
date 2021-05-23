28a85d046d9862224cca89b2916cd93f27898a6b
Kaia <kbergmanndean@gmail.com>
2021-05-22 20:50:24 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.querySelector("form").addEventListener('submit',(event)=> {
+  event.preventDefault()
+  let formObject= {
+    task: event.target.newTask.value
+  }
+  renderForm(formObject)
+})
+
+const renderForm = (tasks) => {
+  let taskList = document.querySelector('#tasks')
+  let li = document.createElement('li')
+  li.textContent=tasks.task
+  taskList.append(li)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 });
